69fece8f0f399483e7db25e2e168b86e772fdfad
achingbrain <alex@achingbrain.net>
2016-12-31 10:28:49 +0000

    Make discover return a promise, also add timeout to wait for services

--- test/lib/discover/index.js
+++ test/lib/discover/index.js
@@ -37,4 +37,32 @@ describe('lib/discover', () => {
     expect(ssdp.emit.getCall(0).args[2].MAN).to.equal('ssdp:discover')
     expect(ssdp.emit.getCall(0).args[2].MX).to.equal(0)
   })
+
+  it('should return services after a timeout', done => {
+    const timeout = 100
+    const ssdp = {
+      emit: sinon.stub(),
+      once: sinon.stub(),
+      on: sinon.stub(),
+      off: sinon.stub()
+    }
+    const serviceType = 'serviceType'
+    const service = {}
+
+    discover(ssdp, serviceType, timeout)
+    .then(services => {
+      expect(services[0]).to.equal(service)
+      expect(ssdp.off.calledOnce).to.be.true
+      expect(ssdp.off.getCall(0).args[0]).to.equal(`discover:${serviceType}`)
+
+      done()
+    })
+
+    setTimeout(() => {
+      expect(ssdp.on.calledOnce).to.be.true
+      expect(ssdp.on.getCall(0).args[0]).to.equal(`discover:${serviceType}`)
+
+      ssdp.on.getCall(0).args[1](service)
+    }, timeout - 10)
+  })
 })
