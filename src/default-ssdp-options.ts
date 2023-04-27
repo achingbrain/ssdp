4daa7419654c15ea36ecca919e7ad2003dc5d61e
Jimmy Wärting <jimmy@warting.se>
2023-04-27 13:58:05 +0200

    fix: remove uuid dependency (#35)
    remove dependency on uuid v4 (as it's built in since node 14)
    
    Co-authored-by: Alex Potsides <alex@achingbrain.net>

--- src/default-ssdp-options.ts
+++ src/default-ssdp-options.ts
@@ -1,19 +1,18 @@
 
+import { webcrypto as crypto } from 'crypto' // remove when having crypto global
 import { createRequire } from 'module'
-import util from 'util'
 import mergeOptions from 'merge-options'
-import { v4 } from 'uuid'
 import { defaultSocketOptions } from './default-socket-options.js'
 import type { SSDPOptions } from './index.js'
 
 const req = createRequire(import.meta.url)
-const pkg = req('../../package.json')
+const { name, version } = req('../../package.json')
 
-const DEFAULT_SSDP_SIGNATURE = util.format('node.js/%s UPnP/1.1 %s/%s', process.version.substring(1), pkg.name, pkg.version)
+const DEFAULT_SSDP_SIGNATURE = `node.js/${process.version.substring(1)} UPnP/1.1 ${name}/${version}`
 
 export function defaultSsdpOptions (options?: Partial<SSDPOptions>): SSDPOptions {
   return mergeOptions(options ?? {}, {
-    usn: `uuid:${v4()}`, // eslint-disable-line @typescript-eslint/restrict-template-expressions
+    usn: `uuid:${crypto.randomUUID()}`, // eslint-disable-line @typescript-eslint/restrict-template-expressions
     signature: DEFAULT_SSDP_SIGNATURE,
     sockets: [{}].map(defaultSocketOptions),
     retry: {
