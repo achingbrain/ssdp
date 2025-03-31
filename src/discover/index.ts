56cbd23f3613a3f23e7f12bc49f929e1a6fbd683
Daniel N <2color@users.noreply.github.com>
2025-03-31 12:41:08 +0200

    fix: set valid MX max wait value like in spec

--- src/discover/index.ts
+++ src/discover/index.ts
@@ -6,6 +6,6 @@ export function discover (ssdp: SSDP, serviceType?: string): void {
   ssdp.emit('ssdp:send-message', 'M-SEARCH * HTTP/1.1', {
     ST: serviceType,
     MAN: '"ssdp:discover"',
-    MX: 0
+    MX: 1
   })
 }
